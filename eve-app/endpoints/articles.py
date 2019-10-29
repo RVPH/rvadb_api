da7164297770237768f9694cb273a122d16e9361
alexmisk <alexmisk@ya.ru>
2019-10-29 18:21:52 +0300

    Move article endpoint logic to separate module

---
+++ eve-app/endpoints/articles.py
@@ -0,0 +1,18 @@
+schema = {
+    'doi': {},
+    'journal': {},
+    'title': {},
+    'authors_list': {},
+    'authors_info': {},
+    'abstract': {},
+    'rubric': {},
+    'keywords': {},
+    'references': {},
+    'pages': {}
+}
+
+settings = {
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+    'schema': schema
+}
--- eve-app/settings.py
+++ eve-app/settings.py
@@ -1,30 +1,13 @@
 import os
 
+import endpoints.articles
+
 MONGO_URI = os.environ['MONGO_URI']
 API_VERSION = 'v1beta1'
 PAGINATION = False
 HATEOAS = False
 SORTING = False
 
-schema = {
-    'doi': {},
-    'journal': {},
-    'title': {},
-    'authors_list': {},
-    'authors_info': {},
-    'abstract': {},
-    'rubric': {},
-    'keywords': {},
-    'references': {},
-    'pages': {}
-}
-
-articles = {
-    'cache_control': 'max-age=10,must-revalidate',
-    'cache_expires': 10,
-    'schema': schema
-}
-
 DOMAIN = {
-    'articles': articles
+    'articles': endpoints.articles.settings
 }
